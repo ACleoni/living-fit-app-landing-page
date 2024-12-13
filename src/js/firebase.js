3eeca2bcbf03b2401caeda2ce26812a2b2e56cad
Alexander Cleoni <alexander.cleoni@gmail.com>
2024-12-13 00:23:34 -0500

    fix exports

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from '@firebase/auth';
 import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+let auth;
+let firestore;
 
 function initializeFirebase(firebaseConfig) {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const firestore = getFirestore(app);
-  module.exports.auth = auth;
-  module.exports.firestore = firestore
+  auth = getAuth(app);
+  firestore = getFirestore(app);
 }
 
-export { initializeFirebase}
+export { initializeFirebase, auth, firestore }
 
