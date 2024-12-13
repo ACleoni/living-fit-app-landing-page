1b9d0d128cacb7a25183ea199703488a5a21a181
Alexander Cleoni <alexander.cleoni@gmail.com>
2024-12-12 22:47:03 -0500

    add additional loggin statements

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -17,6 +17,8 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID,
 };
 
+console.log("firebase config", firebaseConfig)
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
